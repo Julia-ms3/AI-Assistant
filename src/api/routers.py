6136557e196b8be045d59b3d0394e134ae2c3ea3
Julia Matviychuk <Matviychukjulia03@gmail.com>
2025-03-19 10:20:45 +0200

    Implemented WebSocket for real-time text assistance

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter, File, UploadFile
+import os
+
+from fastapi import APIRouter, File, UploadFile, WebSocket, WebSocketDisconnect
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 from src.api.services import image_response, text_response
 
@@ -13,3 +17,26 @@ def generate_ai_response(request: str):
 @router.post('/upload_image')
 def upload_image(request, img_file: UploadFile = File(...)):
     return image_response(request, img_file)
+
+
+router.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@router.get("/")
+async def get_home():
+    return FileResponse(os.path.join("static", "index.html"))
+
+
+@router.websocket("/wb")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"you:  {data}")
+
+            response = await text_response(data)
+            await websocket.send_text(f"assistance: {response}")
+
+    except WebSocketDisconnect:
+        print('Disconnect')
--- src/api/services.py
+++ src/api/services.py
@@ -11,7 +11,7 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv('GOOGLE_API_KEY'))
 
 
-def text_response(content):
+async def text_response(content):
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=[content])
